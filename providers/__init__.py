20bbdb95e2ee9c31609171738e0644b22f9d1165
Exfakto <cyberskycs@gmail.com>
2026-07-01 21:01:04 +0200

    v2.9: Export provider interfaces

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,9 +1,11 @@
 from providers.base_provider import BaseProvider
 from providers.local_provider import LocalProvider
+from providers.provider_manager import ProviderManager
 from providers.provider_result import ProviderResult
 
 __all__ = [
     "BaseProvider",
     "LocalProvider",
+    "ProviderManager",
     "ProviderResult",
 ]
